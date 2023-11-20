3bcca0d3e2b996a5955563a5444962016fdd9b7b
Sahani Anuttara <96613311+shanujay@users.noreply.github.com>
2023-11-20 12:25:50 +0530

    Help Center Activity

--- app/src/main/java/com/techcools/gostudy/HelpCenterActivity.java
+++ app/src/main/java/com/techcools/gostudy/HelpCenterActivity.java
@@ -1,14 +1,34 @@
 package com.techcools.gostudy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class HelpCenterActivity extends AppCompatActivity {
 
+    // creating object of ViewPager
+    ViewPager mViewPager;
+
+    // images array
+    int[] images = {R.drawable.pomodoro_timer_ui, R.drawable.study_music_ui,
+            R.drawable.questionnaire_ui};
+
+    // Creating Object of ViewPagerAdapter
+    HelpCenterViewPagerAdapter helpCenterViewPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_center);
+
+        // Initializing the ViewPager Object
+        mViewPager = (ViewPager)findViewById(R.id.viewPagerMain);
+
+        // Initializing the ViewPagerAdapter
+        helpCenterViewPagerAdapter = new HelpCenterViewPagerAdapter(HelpCenterActivity.this, images);
+
+        // Adding the Adapter to the ViewPager
+        mViewPager.setAdapter(helpCenterViewPagerAdapter);
     }
 }
