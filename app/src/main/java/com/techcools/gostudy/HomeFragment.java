72008adadf2082bbfc3a3b70c6bfafa53c94d364
sandsletsCode <110994264+sandsletsCode@users.noreply.github.com>
2023-11-22 20:54:02 +0530

    home page

--- app/src/main/java/com/techcools/gostudy/HomeFragment.java
+++ app/src/main/java/com/techcools/gostudy/HomeFragment.java
@@ -7,13 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class HomeFragment extends Fragment {
+
+    ImageButton pomodoroTimerBtn;
+    ImageButton studyMethodBtn;
+    ImageButton UpcomingTaskBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        pomodoroTimerBtn = (ImageButton) view.findViewById(R.id.Pomodoro_timerBtn);
+        pomodoroTimerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return view;
     }
 }
+
